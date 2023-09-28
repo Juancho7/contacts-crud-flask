049b1b0c1d042e0045188cfcae10ef5e1420c062
juancho7 <juanfertrom@gmail.com>
2023-09-28 18:30:29 -0500

    002: adding contacts function was added002: adding contacts function was added

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
+from MySQLdb.cursors import DictCursor
 
 app = Flask(__name__)
 
@@ -7,7 +8,7 @@ app.secret_key = "your secret key"
 
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = "password"
+app.config["MYSQL_PASSWORD"] = "root"
 app.config["MYSQL_DB"] = "flask_basics"
 
 mysql = MySQL(app)
@@ -15,11 +16,32 @@ mysql = MySQL(app)
 
 @app.route("/")
 def index():
-    return "Hello world"
+    cursor = mysql.connection.cursor(DictCursor)
+    cursor.execute("SELECT * FROM contacts")
+    contacts_data = cursor.fetchall()
+    cursor.close()
+    print(contacts_data)
+    return render_template("index.html", contacts=contacts_data)
 
 
-@app.route("/add")
+@app.route("/add", methods=["POST"])
 def add_contact():
+    if request.method == "POST":
+        fullname = request.form["fullname"]
+        phone = request.form["phone"]
+        email = request.form["email"]
+        print(fullname, phone, email, "Prueba")
+        try:
+            cursor = mysql.connection.cursor()
+            cursor.execute(
+                "INSERT INTO contacts (fullname, phone, email) VALUES (%s, %s, %s)",
+                (fullname, phone, email),
+            )
+            mysql.connection.commit()
+            return redirect(url_for("index"))
+        except Exception as e:
+            raise e
+
     return "Add contact"
 
 
